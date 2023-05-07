d4107a453c86eec553df8abcb22e276acb1a12c5
yassmine <yassminemohamad2001@gmail.com>
2023-05-07 21:24:37 +0300

    cors

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
+const cors = require('cors');
 const customerController = require('./../controllers/customerController');
 const authController = require('./../controllers/authController');
+
 const factory = require('./../controllers/handlerFactory');
 
 const Workshop = require('./../models/workshopModel');
@@ -10,7 +12,7 @@ const Customer = require('./../models/customerModel');
 const Mechanic = require('./../models/mechanicModel');
 
 router.post('/forgotpassword', authController.forgotPassword);
-router.post('/login', authController.logIn);
+router.post('/login', cors(), authController.logIn);
 router.post('/signup', authController.signUp);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
