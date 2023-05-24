eca9078a31e4067a44f49e9651039672ce50339f
yassmine <yassminemohamad2001@gmail.com>
2023-05-24 06:40:54 +0300

    -

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const multer = require('multer');
 const { promisify } = require('util');
 const catchAsync = require('./../utils/catchAsync');
 const jwt = require('jsonwebtoken');
@@ -11,6 +12,27 @@ const crypto = require('crypto');
 const { isDate } = require('util/types');
 const Review = require('../models/reviewModel');
 // const { Model } = require('mongoose');
+
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/img/users');
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split('/')[1];
+    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+  },
+});
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('not an image ,please choose an image', 400), false);
+  }
+};
+const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
+
+exports.uploadUserImage = upload.single('image');
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
