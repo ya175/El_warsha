f9867ce08b4e297034cfa82955c7b0d7fca10a31
yassmine <yassminemohamad2001@gmail.com>
2023-07-05 02:26:19 +0300

    cookie

--- app.js
+++ app.js
@@ -19,9 +19,28 @@ const carRouter = require('./routes/carRoutes');
 
 const app = express();
 
-app.use(cors({ credentials: true }));
-app.options('*', cors());
+// app.use(cors({ credentials: true }));
+// app.options('*', cors());
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+};
 
+app.use(cors(corsOptions));
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
