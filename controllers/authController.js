27d0adba2cad8e651680b7bd66b512a8b59d3285
yassmine <yassminemohamad2001@gmail.com>
2023-07-05 05:29:37 +0300

    cookie10

--- controllers/authController.js
+++ controllers/authController.js
@@ -40,7 +40,7 @@ const createSendToken = (user, req, statusCode, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     // sameSite: 'none',
-    secure: false,
+    secure: true,
     httpOnly: false,
     // secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
 
