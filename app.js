3574a32d2a6a8e8bbd97272b0eababb51017e303
yassmine <yassminemohamad2001@gmail.com>
2023-07-05 22:11:41 +0300

    cookie16

--- app.js
+++ app.js
@@ -20,18 +20,18 @@ const carRouter = require('./routes/carRoutes');
 const app = express();
 
 var corsOptions = {
-  // origin: 'http://localhost:3000',
+  origin: '*',
   credentials: true,
 };
 
 app.use(cors(corsOptions));
 
 app.use(function (req, res, next) {
-  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  );
+  res.setHeader('Access-Control-Allow-Origin', '*'),
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+    );
   res.setHeader(
     'Access-Control-Allow-Headers',
     'X-Requested-With,content-type'
