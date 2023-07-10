fb9e3f66e8e42cff3c38bc2af751c904b49d8dd4
yassmine <yassminemohamad2001@gmail.com>
2023-07-11 00:06:00 +0300

    product images_

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -79,7 +79,7 @@ router.get('/', async (req, res) => {
       products = await Product.find();
     }
 
-    res.status(200).json(products);
+    res.status(200).json({ data: products });
   } catch (err) {
     res.status(500).json(err);
   }
